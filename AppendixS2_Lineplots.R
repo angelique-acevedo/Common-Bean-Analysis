5aa4a2eb3f09ef51c6f6feb03399a3c0e7ce9f0f
Angelique Acevedo <liqacevedo@gmail.com>
2025-02-27 19:08:07 -0600

    AppendixS2_Lineplots.R
    #DESCRIPTION: This script is generated to produce linegraphs with standard error bars tracking internode development in Appendix S2 of Onyenedum el al. 2025.

--- AppendixS2_Lineplots.R
+++ AppendixS2_Lineplots.R
@@ -1,6 +1,6 @@
-#Generated with PI Joyce Onyenedum (Onyenedum Lab)
-#This script is generated to produce linegraphs displaying three light treatment groups from plastochron stage 0 to 9. A line graph for each internode development was made individually.  
-l
+#Generated with Dr. Joyce Onyenedum (Onyenedum Lab)
+#DESCRIPTION: This script is generated to produce linegraphs with standard error bars tracking internode development in Appendix S2 of Onyenedum el al. 2025. 
+
 ibrary(devtools)
 library(multcompView)
 library(ggplot2)
@@ -9,13 +9,17 @@ library(ggpubr)
 library(plyr)
 #library("scales")
 
-#Each internode has its own .csv throughout its development, however the file provided is a composite of all internode development named "RawMeasurements...". To generate the isloted graphs below, we seperated each internode and its corresponding measurements into its own .csv file but with the same formatting (column names and variables) as the one in the composite file.
+#Import composite dataset
 datum <- read.csv("~/Library/)
 
+#Use the function below to filter the composite dataset and generate a line graph of the segment of your choice. 
+datum1<- subset(datum, Segment== "IN9") 
 
-datum$Time_Point <- factor(datum$Time_Point,levels = c("P0", "P1", "P2","P3", "P4","P5", "P6","P7","P8","P9"))
-datum$Treatment <- factor(datum$Treatment,levels = c("L88 57 Low Light", "L88 57 High Light", "Zenith Low Light"))
-datum <- na.omit (datum)
+#Functions below are used to organize dataset into 
+datum1$Time_Point <- factor(datum1$Time_Point,levels = c("P0", "P1", "P2","P3", "P4","P5", "P6","P7","P8","P9"))
+datum1$Treatment <- factor(datum1$Treatment,levels = c("L88 57 Low Light", "L88 57 High Light", "Zenith Low Light"))
+datum1 <- na.omit (datum1)
+
 
 #Calculate summary statistics
 data_summary <- function(data, varname, groupnames){
@@ -31,13 +35,13 @@ data_summary <- function(data, varname, groupnames){
   return(data_sum)
 }
 
-#Generate line plot with SE bars at each internode 
-datum<- na.omit (datum)
-head(datum$Length)
-df3 <- data_summary(datum, varname="Length", groupnames=c("Treatment", "Plastochron"))
+datum1<- na.omit (datum1)
+head(datum1$Length)
+df3 <- data_summary(datum1, varname="Length", groupnames=c("Treatment", "Plastochron"))
 
 pd <- position_dodge(0.3) 
 
+#Generate line plot with SE bars at each plastochron 
 p<- ggplot(df3, aes(x=Plastochron, y= Length, colour=Treatment, group=Treatment)) + 
   geom_errorbar(aes(ymin= Length-se, ymax= Length+se), colour="black", width=.1, position=pd) +
   geom_line(position=pd) +
@@ -51,5 +55,6 @@ p<- ggplot(df3, aes(x=Plastochron, y= Length, colour=Treatment, group=Treatment)
                              axis.title.y= element_text(size = 15), legend.position = "top") + theme_bw(base_size = 20) +
 ylim(0,10)
 
+#Set color scheme for three treatment groups 
 p<- change_palette(p, c("#648FFF", "#EC632B", "#73C956"))
 p
